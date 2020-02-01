f53cbfe69b9949f78892644dd2d4785f942828b5
ryan <17805315@student.westernsydney.edu.au>
2020-02-02 08:49:30 +1100

    Added Math Live Preview

--- Spacemacs/.emacs.d/init.el
+++ Spacemacs/.emacs.d/init.el
@@ -58,6 +58,55 @@
 (add-hook 'sh-mode-hook 'flycheck-mode)
 (add-hook 'sh-mode-hook 'company-mode)
 (global-set-key (kbd "C-c v") 'open-term-copy-path)
+;;;;; Auto Preview Math
+
+
+(defvar org-equation-live-preview-buffer-name "*Equation Live*")
+(defvar org-equation-live-preview-split-size 30)
+(defvar org-equation-live-preview-idle-time 0.5)
+(defvar org-equation-live-preview--timer)
+(defvar org-equation-live-preview--org-buffer)
+(defvar org-equation-live-preview--preview-buffer)
+
+(defun org-equation-live-preview--preview ()
+  (interactive)
+  (let ((datum (org-element-context)))
+    (and (memq (org-element-type datum) '(latex-environment latex-fragment))
+         (let* ((beg (org-element-property :begin datum))
+                (end (org-element-property :end datum))
+                (text (buffer-substring-no-properties beg end)))
+           (with-current-buffer org-equation-live-preview--preview-buffer
+             (let ((inhibit-read-only t))
+               (erase-buffer) (insert (replace-regexp-in-string "\n$" "" text))
+               (org-preview-latex-fragment)))))))
+
+(defun org-equation-live-preview--open-buffer ()
+  (interactive)
+  (setq org-equation-live-preview--org-buffer (current-buffer))
+  (let ((buffer-name org-equation-live-preview-buffer-name))
+    (setq org-equation-live-preview--preview-buffer
+          (get-buffer-create buffer-name))
+    (split-window-vertically org-equation-live-preview-split-size)
+    (other-window 1)
+    (switch-to-buffer buffer-name)
+    (special-mode)
+    (other-window -1)))
+
+(defun org-equation-live-preview--activate ()
+  (interactive)
+  (org-equation-live-preview--open-buffer)
+  (message "Activating org-equation-live-preview")
+  (setq org-equation-live-preview--timer
+        (run-with-idle-timer org-equation-live-preview-idle-time t 'org-equation-live-preview--preview)))
+
+(defun org-equation-live-preview--deactivate ()
+  (interactive)
+  (delete-other-windows)
+  (message "Deactivating org-equation-live-preview")
+  (cancel-function-timers 'org-equation-live-preview--preview))
+
+(provide 'org-equation-live-preview)
+
 ;;;; AutoExport OrgMode to HTML
 (defun toggle-html-export-on-save ()
   "Enable or disable export HTML when saving current buffer."
