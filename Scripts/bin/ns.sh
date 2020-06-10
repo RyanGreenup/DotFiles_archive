ce2235af2e3bf6ecb53558a68b30bab17b96589c
ryangreenup <exogenesis@protonmail.com>
2020-06-11 09:30:05 +1000

    Added -b flack to skim search

--- Scripts/bin/ns.sh
+++ Scripts/bin/ns.sh
@@ -9,4 +9,12 @@ echo searching for $query
 
 # recoll -b -t -q "ext:md $query" | cut -c 8- |  fzf --preview "highlight -O ansi -l {}" | xargs code -a
 # recoll -b -t -q "ext:md $query" | cut -c 8- |  fzf --bind up:preview-up,down:preview-down --preview "mdcat {}" | xargs code -a
-recoll -b -t -q "ext:md $query" | cut -c 8- |  fzf --bind pgup:preview-page-up,pgdn:preview-page-down --preview "mdcat {}" | xargs code -a
+ recoll -b -t -q "ext:md $query" | cut -c 8- |  fzf --bind pgup:preview-page-up,pgdn:preview-page-down --preview "mdcat {}" | xargs code -a
+
+ ## You can get a live search, it's actually not bad try this:
+#sk --ansi -i -c 'recoll -b -t "{}ext:md" | cut -c 8-' --preview "mdcat {}"
+#
+## but after the search you then want to browse through the file names anyway,
+## hmm
+# or with rg
+## sk --ansi -i -c 'rg --color=always --line-number "{}"'
