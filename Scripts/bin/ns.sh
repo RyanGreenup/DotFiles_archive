88bc1fa6773a760cda8963c60ef493bbf7314ba8
ryangreenup <exogenesis@protonmail.com>
2020-06-10 21:49:58 +1000

    Changed to MDCat for Note Search

--- Scripts/bin/ns.sh
+++ Scripts/bin/ns.sh
@@ -7,4 +7,6 @@ echo Enter the Search Query
 read -p 'Search: ' query
 echo searching for $query
 
-recoll -b -t -q "ext:md $query" | cut -c 8- |  fzf --preview "highlight -O ansi -l {}" | xargs code -a
+# recoll -b -t -q "ext:md $query" | cut -c 8- |  fzf --preview "highlight -O ansi -l {}" | xargs code -a
+# recoll -b -t -q "ext:md $query" | cut -c 8- |  fzf --bind up:preview-up,down:preview-down --preview "mdcat {}" | xargs code -a
+recoll -b -t -q "ext:md $query" | cut -c 8- |  fzf --bind pgup:preview-page-up,pgdn:preview-page-down --preview "mdcat {}" | xargs code -a
