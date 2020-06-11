7f8bb86927d2c8766959a10ff06e2b5b50cb1be2
ryan <17805315@student.westernsydney.edu.au>
2020-06-11 12:28:37 +1000

    Modified to try and have a live recoll search

--- Scripts/bin/ns.sh
+++ Scripts/bin/ns.sh
@@ -1,20 +1,20 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/bash
 
 command -v highlight >/dev/null 2>&1 || { echo >&2 "The highlight package is a required dependency"; exit 1; }
+command -v mdcat     >/dev/null 2>&1 || { echo >&2 "The mdcat package is a required dependency, install it with ~cargo install mdcat~"; exit 1; }
+command -v sk        >/dev/null 2>&1 || { echo >&2 "The skim package is a required dependency, install it with ~cargo install skim~"; exit 1; }
 
-echo Enter the Search Query
-read -p 'Search: ' query
-echo searching for $query
 
-# recoll -b -t -q "ext:md $query" | cut -c 8- |  fzf --preview "highlight -O ansi -l {}" | xargs code -a
-# recoll -b -t -q "ext:md $query" | cut -c 8- |  fzf --bind up:preview-up,down:preview-down --preview "mdcat {}" | xargs code -a
- recoll -b -t -q "ext:md $query" | cut -c 8- |  fzf --bind pgup:preview-page-up,pgdn:preview-page-down --preview "mdcat {}" | xargs code -a
 
- ## You can get a live search, it's actually not bad try this:
-#sk --ansi -i -c 'recoll -b -t "{}ext:md" | cut -c 8-' --preview "mdcat {}"
-#
-## but after the search you then want to browse through the file names anyway,
-## hmm
-# or with rg
-## sk --ansi -i -c 'rg --color=always --line-number "{}"'
+if [ "$1" == "-l" ]; then
+    sk --ansi -i -c 'recoll -b -t "{}ext:md" | cut -c 8-' --preview "mdcat {}" \
+        --bind pgup:preview-page-up,pgdn:preview-page-down
+else
+    echo Enter the Search Query
+    read -p 'Search: ' query
+    echo searching for $query
+
+    recoll -b -t -q "ext:md $query" | cut -c 8- | \
+        fzf --bind pgup:preview-page-up,pgdn:preview-page-down --preview "mdcat {}" \
+        | xargs code -a
+fi
