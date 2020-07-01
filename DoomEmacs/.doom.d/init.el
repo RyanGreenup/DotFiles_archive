cd705f68b599c69aed4284e71dc1e5a4468612f0
ryangreenup <exogenesis@protonmail.com>
2020-07-01 23:29:16 +1000

    Keybinding and enable vterm

--- DoomEmacs/.doom.d/init.el
+++ DoomEmacs/.doom.d/init.el
@@ -41,7 +41,7 @@
        pretty-code       ; ligatures or substitute text with pretty symbols
        ;;tabs              ; an tab bar for Emacs
        treemacs          ; a project drawer, like neotree but cooler
-       unicode           ; extended unicode support for various languages
+       ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        window-select     ; visually switch windows
@@ -73,7 +73,7 @@
          eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       ;;vterm             ; the best terminal emulation in Emacs
+       vterm             ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
@@ -144,7 +144,7 @@
        ;;ocaml             ; an objective camel
        (org +roam
             ;; +hugo
-            ;; +brain
+             ;; +brain
             ;; +gnuplot
              +ipython
              +pandoc
