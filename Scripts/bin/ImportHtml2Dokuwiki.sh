6c8e60537fb87a0821f9aa7c3c07ff2e7da6960e
Ryan Greenup <exogenesis@protonmail.com>
2020-09-06 12:50:24 +1000

    Fixed Import Script for dokuwiki

--- Scripts/bin/ImportHtml2Dokuwiki.sh
+++ Scripts/bin/ImportHtml2Dokuwiki.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
+# * Preamble
+    readonly script_name=$(basename "${0}")
+             script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+    readonly script_dir=$(realpath "${script_dir}""/""${script_name}" | xargs dirname)
 # * Check Arguments and write Permissions
 
 if [[ "${1}" == "--help" || "${1}" == "-h" ]]; then
-    echo -e "First argument should be a HTML File"
+    echo -e "First argument should be a HTML File in the current directory"
     echo -e "Second argument is optional and should be the namespace HTML File"
     echo -e "\tDownloaded with Firefox Worked for me"
     echo -e "This script will take a HTML file and return a data directory that can be merged"
     echo -e "with dokuwiki such that relative path images will work"
     exit 0
 elif [[ "${1}" == "" ]]; then
-    echo "Please provide the HTML File as the first argument"
+    echo -e "\n Please provide the HTML File as the first argument\n"
+    "${script_dir}"/"${script_name}" -h
     exit 0
 elif [[ ! -f "${1}" ]]; then
     echo "Cannot detect any file by that name in this directory"
@@ -68,27 +73,22 @@ mkdir -p "${MEDIA_DIR}"
 # *** Change Directories into the Media Directory
 cd data/media/"${NSPC}"
 # *** Call Pandoc to Extract Media
-pandoc "${INPUT}" -f html --extract-media ./ >& /dev/null
+# pandoc creates a directory called media
+  # beware the rel path on --extract-media!
+    # Currently pandoc creates a media directory for all rel paths
+    # if this changes this script will need to change
+    # TODO I think a more robust solution would be to use sed
+        # So give pandoc the abs path to the media directory
+        # then use sed to fix the links inside the dokuwiki txt file
+        # this way any changes in where media is stored by dokuwiki or pandoc
+        # will not affect this script
+  # html math should work fine in dokuwiki because
+  # pandoc doesn't escape it.
+pandoc "${INPUT}" -f html -t dokuwiki --extract-media="./" -o "${OUTPUT_NAME}"
 # ** Create the Corresponding Page
 # *** Change into the pages directory
-cd "${DIR}/${PAGE_DIR}"
-# *** Convert the Document
-pandoc "${INPUT}" -f html -t dokuwiki --extract-media ./ -o "${OUTPUT_NAME}"
-# *** Store the Document in the clipboard
-DWTMP="$(mktemp)"
-mv  "${OUTPUT_NAME}" "${DWTMP}"
-# *** Delete Everything
-# Delete all the extracter material that belongs in a different directory
-# The file has to be saved in the directory because otherwise the links
-# will not be altered so there's gotta be a paste and delete without
-# fundamentally modifying pandoc
-rm -rf *
-# Put the clipboard back into the directory
-mv "${DWTMP}" "${OUTPUT_NAME}"
+mv "${OUTPUT_NAME}" "${DIR}/${PAGE_DIR}"
 
-
-# Change directories back to root.
-cd "${DIR}"
 
 
 # Should the user continue?
@@ -97,17 +97,24 @@ echo -e "This will alsow chown -R http:http /srv/http/dokuwiki \n"
 echo -e "Press y to continue and any other key to exit"
 read -d '' -s -n1 continueQ
 if [ ! "${continueQ}" == "y" ]; then
+echo -e "Exported Data Directory Available in:\n ${DIR}"
     exit 0
 fi
 
-echo "Exported Data Directory Available in ${DIR}"
+
 
 # * Copy the data into Dokuwiki
+# Change directories back to root.
+cd "${DIR}"
 # To copy the data into mediawiki
 # Merge the data directory with the dokuwiki one
+sudo chown http:http -R data
 sudo cp -r data/ /srv/http/dokuwiki/
 # Change ownership of the files
 sudo chown -R http:http /srv/http/dokuwiki
+echo -e "\nMake sure to run:\n"
+echo -e "sudo chown -R http:http /srv/http/dokuwiki \n"
+
 
 echo "Successfully imported HTML File into Dokuwiki"
 
