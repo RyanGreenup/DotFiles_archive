34ae38fc13c6ee5ba8992618da62dedf67879f54
Ryan Greenup <exogenesis@protonmail.com>
2020-09-26 00:42:11 +1000

    Added Oxford LaTeX Template

---
+++ Templates/Templates/LaTeX/codestyles/.auctex-auto/barline_listings.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook
+ "barline_listings"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "listings"
+    "xcolor"))
+ :latex)
+
