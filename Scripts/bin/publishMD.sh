3f33f8b616164c731afbfb27261d587eec949160
Ryan Greenup <exogenesis@protonmail.com>
2020-10-04 09:35:24 +1000

    Fix PublishMD Snippet

--- Scripts/bin/publishMD.sh
+++ Scripts/bin/publishMD.sh
@@ -8,32 +8,36 @@
 #
 
 ## Define Server and Notes Location
-Server=/home/ryan/Public/html
+Server="/srv/http"
 Notes=/home/ryan/Notes # Location of mkdocs.yml
+MKDOCS="/home/ryan/.local/bin/mkdocs"
+TEMP="$(mktemp -d)"
 
 ## Change Directory
-cd $Notes/
-rm -r ~/Notes/MD/00TagMatch
+cd "${Notes}/"
+rm -r "${Notes}/MD/00TagMatch"
 
 ## Build the Documentation
 ##
 ##
 if [ "$1" == "-h" ]; then
   echo "Usage: `basename $0`
-             # -c or --clean for clean build, defaults to --dirty"
+             # -d or --dirty for dirty build, defaults to --clean"
   exit 0
 elif [[ "$1" == *-d* || "$1" == *--dirty* ]]; then
-/home/ryan/.local/bin/mkdocs build --dirty --site-dir $Server/MD/
+ "${MKDOCS}" build --dirty --site-dir "${TEMP}/MD/"
+ sudo mv "${TEMP}/MD/" "${Server}/MD"
 exit 0
 else
-/home/ryan/.local/bin/mkdocs build --clean --site-dir $Server/MD/
+ "${MKDOCS}" build --clean --site-dir "${TEMP}/MD/"
+ sudo mv "${TEMP}/MD/" "${Server}/MD"
 exit 0
 fi
 ## TODO Clean, Dirty
-# /home/ryan/.local/bin/mkdocs build --dirty --site-dir $Server/MD/
+# "${MKDOCS}" build --dirty --site-dir $Server/MD/
 
 
 ## cd /home/ryan/Notes/
-## /home/ryan/.local/bin/mkdocs build
+## "${MKDOCS}" build
 ##  [ -d "/home/ryan/Public/html/MD" ] && sudo rm -r /home/ryan/Public/html/MD
 ##  [ -d "./site" ] && sudo mv site /home/ryan/Public/html/MD/
