3d03dce27d06f4a1ee1ca3f2e2531c82fc8caa2a
Ryan Greenup <exogenesis@protonmail.com>
2021-01-23 11:26:34 +1100

    Add wiki search alias fish

---
+++ fish/.config/fish/functions/ws.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function ws --wraps=/home/ryan/bin/dokuwiki_search.sh --description 'alias ws=/home/ryan/bin/dokuwiki_search.sh'
+  /home/ryan/bin/dokuwiki_search.sh  $argv;
+end
