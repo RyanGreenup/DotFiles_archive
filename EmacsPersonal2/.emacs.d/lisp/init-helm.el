b206b2433db320c54dd671d641f70940570bbff6
Ryan Greenup <exogenesis@protonmail.com>
2021-01-29 13:05:30 +1100

    Add Function to Recursivly find files in helm

--- EmacsPersonal2/.emacs.d/lisp/init-helm.el
+++ EmacsPersonal2/.emacs.d/lisp/init-helm.el
@@ -5,9 +5,16 @@
 
 (global-set-key (kbd "M-x") 'helm-M-x)
 (evil-define-key 'normal 'global (kbd "<leader>ff") 'helm-find-files) ; Use helm
+(evil-define-key 'normal 'global (kbd "<leader>fF") 'my/helm-find-file-recursively) 
 (evil-define-key 'normal 'global (kbd "<leader>fr") 'helm-recentf) ;; use helm
 (evil-define-key 'normal 'global (kbd "<leader>ss") 'helm-swoop)
 
+(defun my/helm-find-file-recursively ()  
+  (interactive)
+  "Recursively find files in glob manner, in the specified directory."
+  (helm-find 'ask-for-dir))
+
+(global-set-key (kbd "C-c o f") 'my/helm-find-file-recursively)
 
 (provide 'init-helm)
 ;;; init-helm.el ends here
