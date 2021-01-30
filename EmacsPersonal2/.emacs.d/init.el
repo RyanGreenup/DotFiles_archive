1b94c3cbfc0a15823554dc49046d0e7d68dc8771
Ryan Greenup <exogenesis@protonmail.com>
2021-01-30 19:00:35 +1100

    Indent and format init file inside after-init hook

--- EmacsPersonal2/.emacs.d/init.el
+++ EmacsPersonal2/.emacs.d/init.el
@@ -16,101 +16,87 @@
 (setq debug-on-error t)
 
 
+;;;; Run After Initialisation
 (add-hook 'after-init-hook (lambda ()
 
-;;;; Load Package Manager
-(require 'init-straight)
+    ;;;;; Load Package Manager
+    (require 'init-straight)
 
-;; (require 'init-elpa)
+    ;; (require 'init-elpa)
 
-(defun display-startup-echo-area-message ()
-  (message (format "Starup Time: %s"(float-time
-		  (time-subtract after-init-time before-init-time))
-	   ) 
-  )
-)
-
-
-;;;; Load all packages
-(require 'init-user-packages)
-(require 'init-keybindings)
-(require 'init-change-theme-timer)
-
-;;;; Configure Evil
-(require 'init-evil)
-
-;;;; Configure Helm
-(require 'init-helm)
-;; (require 'init-ivy)
-
-;;;; Configure Org Mode
-;; This contributes to a significant amount of startup time
-;; do it after initializing
-(add-hook 'after-init-hook
-	  (lambda ()
-		(require 'init-org)
-	    )
-	  ) ;; "HACK" This should be restructured inside init-org
-
-(require 'init-texfrag)
-
-;; Set default font and apply theme
+    (defun display-startup-echo-area-message ()
+    (message (format "Starup Time: %s"(float-time
+		    (time-subtract after-init-time before-init-time))
+	    ) 
+    )
+    )
 
 
+    ;;;;; Load all packages
+    (require 'init-user-packages)
+    (require 'init-keybindings)
+    (require 'init-change-theme-timer)
 
-    (message "Begining Post Initialisation Modifications")
-    ;; (set-frame-font "Roboto Mono-10" nil t)
-    (set-frame-font "Fira Code-10" nil t)
-    ;; (set-frame-font "monofur-10" nil t)
-    ;; (set-frame-font "Source Code Pro-10" nil t)
-    ;; Toggle scroll bar and Toolbar 
-    (toggle-scroll-bar -1) 
-    (tool-bar-mode -1) 
-    ;; (menu-bar-mode -1)  ;; I like the Menus actually
+    ;;;;; Configure Evil
+    (require 'init-evil)
+
+    ;;;;; Configure Helm
+    (require 'init-helm)
+    ;; (require 'init-ivy)
+
+    ;;;;; Configure Org Mode
+    ;; This contributes to a significant amount of startup time
+    ;; do it after initializing
+    (add-hook 'after-init-hook
+	    (lambda ()
+		    (require 'init-org)
+		)
+	    ) ;; "HACK" This should be restructured inside init-org
+
+    (require 'init-texfrag)
+
+    ;;;;; Set default font and apply theme
+    ;; BEGIN_SLOW These should definitely be wrapped in eval-after-init atleast
+	(message "Begining Post Initialisation Modifications")
+	;; (set-frame-font "Roboto Mono-10" nil t)
+	(set-frame-font "Fira Code-10" nil t)
+	;; (set-frame-font "monofur-10" nil t)
+	;; (set-frame-font "Source Code Pro-10" nil t)
+	;; Toggle scroll bar and Toolbar 
+	(toggle-scroll-bar -1) 
+	(tool-bar-mode -1) 
+	;; (menu-bar-mode -1)  ;; I like the Menus actually
 
 
-    (texfrag-global-mode 1) ;; TODO Move this in with helm
-    (winner-mode 1) ;; TODO Move this in with helm
-    ;; Set Theme
-    (set-theme-for-time-of-day)
-
+	(texfrag-global-mode 1) ;; TODO Move this in with helm
+	(winner-mode 1) ;; TODO Move this in with helm
+	;; Set Theme
+	(set-theme-for-time-of-day)
+    ;; END_SLOW 
 
 
 
 
-;; Doom Modeline
-;; (require 'init-doom-modeline)
 
-;; Hydra
-(add-hook 'after-init-hook
-	  (lambda ()
-	    (require 'init-hydra)
-	    )
-	  )
+    ;;;;; Doom Modeline
+    ;; This causes major scrolling and performance issues in org-mode
+    ;; (require 'init-doom-modeline)
 
-;; Auto Save
-;; Save when actually idle, this may get annoying though
-;; (run-with-idle-timer 5 t 
-;; 		     (lambda ()
-;; 		       (save-buffer)
-;; 		       (message "Saved")
-;; 		       )
-;; 		     )
-;; compile hook
-;; (add-hook 'after-save-hook '(lambda () (byte-recompile-directory "~/.emacs.d/lisp")) t t)
+    ;;;;; Hydra
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(helm-source-names-using-follow '("Buffers")))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+    ;; Auto Save
+    ;; Save when actually idle, this may get annoying though
+    ;; (run-with-idle-timer 5 t 
+    ;; 		     (lambda ()
+    ;; 		       (save-buffer)
+    ;; 		       (message "Saved")
+    ;; 		       )
+    ;; 		     )
+    ;; compile hook
+    ;; (add-hook 'after-save-hook '(lambda () (byte-recompile-directory "~/.emacs.d/lisp")) t t)
+
+
+
 
 ))
 
