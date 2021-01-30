9c295552eeff942db3d5934a66c1301648342046
Ryan Greenup <exogenesis@protonmail.com>
2021-01-30 17:32:41 +1100

    Can now use M-n/M-p to swap lines

--- EmacsPersonal2/.emacs.d/lisp/init-keybindings.el
+++ EmacsPersonal2/.emacs.d/lisp/init-keybindings.el
@@ -7,6 +7,27 @@
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
 (global-set-key (kbd "C-c C-p C-p") 'texfrag-force-preview-at-point)
 
+(global-set-key (kbd "M-p")  'move-line-up)
+(global-set-key (kbd "M-n")  'move-line-down)
+
+
+
+
+
+(defun move-line-up ()
+  "Move up the current line."
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2)
+  (indent-according-to-mode))
+
+(defun move-line-down ()
+  "Move down the current line."
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1)
+  (indent-according-to-mode))
 
 (provide 'init-keybindings)
 ;;; init-keybindings.el ends here
