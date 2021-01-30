717d46c470046b448d136217dd55b418dc91b671
Ryan Greenup <exogenesis@protonmail.com>
2021-01-30 19:28:46 +1100

    Added Comments to popup

--- EmacsPersonal2/.emacs.d/lisp/init-popup-scratch.el
+++ EmacsPersonal2/.emacs.d/lisp/init-popup-scratch.el
@@ -3,26 +3,29 @@
 ;;; Code:
 
 
-(defvar init-is-pop-scratch)
-(setq init-is-pop-scratch nil)
+(defvar init-is-pop-scratch-up)
+(setq init-is-pop-scratch-up nil)
 (defvar init-my-scratch-file "~/Sync/Scratch")
+
+
 (defun pop-scratch ()
   "Pop up a scratch Buffer"
     (interactive)
-    (if init-is-pop-scratch
+    (if init-is-pop-scratch-up
         (close-popup-scratch)
         (open-popup-scratch)
     )
 )
 
 (defun open-popup-scratch ()
-  (setq init-is-pop-scratch t)
+  (setq init-is-pop-scratch-up t)
   (find-file-noselect init-my-scratch-file)
   (popwin:popup-buffer "*scratch*")
+  ;; (popwin:popup-buffer init-my-scratch-file)
 )
 
 (defun close-popup-scratch ()
-  (setq init-is-pop-scratch nil)
+  (setq init-is-pop-scratch-up nil)
   (popwin:close-popup-window)
   )
 
