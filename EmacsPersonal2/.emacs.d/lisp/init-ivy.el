b62593b698858d458ec365785eb0a995a05f4c55
Ryan Greenup <exogenesis@protonmail.com>
2021-02-03 10:45:09 +1100

    Build up ivy settings

--- EmacsPersonal2/.emacs.d/lisp/init-ivy.el
+++ EmacsPersonal2/.emacs.d/lisp/init-ivy.el
@@ -6,6 +6,21 @@
 ;; (global-set-key (kbd "M-x") 'ivy-M-x)
 (evil-define-key 'normal 'global (kbd "<leader>ff") 'counsel-find-file) ; Use ivy
 (evil-define-key 'normal 'global (kbd "<leader>fr") 'consult-recent-files) ;; use ivy
+(evil-define-key 'normal 'global (kbd "<leader>fz") 'counsel-fzf) ;; use ivy
+(evil-define-key 'normal 'global (kbd "<leader>/") 'counsel-rg) ;; use ivy
+(evil-define-key 'normal 'global (kbd "<leader>fd") 'counsel-fd) ;; use ivy
+(evil-define-key 'normal 'global (kbd "<leader>ss") 'swiper) ;; use ivy
+(global-set-key (kbd "M-x") 'counsel)
+
+
+(setq helm-swoop-pre-input-function (lambda () ""))
+;; C-M-p and C-M-n should work like follow mode
+
+
+
+
+
+
 
 ;; (ivy-mode 1)
 
