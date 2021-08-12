ba11a3e1eac4d5069eb63e943ad611ace92c9e9a
Ryan G <exogenesis@protonmail.com>
2021-08-12 20:44:44 +1000

    [polybar]: Open on all available monitors

--- bspwm/.config/polybar/launch.sh
+++ bspwm/.config/polybar/launch.sh
@@ -6,7 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar --reload topbar  & # Reload after config
+# Query every monitor [1]
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload topbar &
+  done
+else
+    # Launch Polybar, using default config location ~/.config/polybar/config
+    polybar --reload topbar  & # Reload after config
+fi
 
 echo "Polybar launched..."
